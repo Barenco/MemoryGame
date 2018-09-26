031a92cb5176851c3dcd9bdd4d695c3cc61bdcdd
barenco <davidbarenco@gmail.com>
2018-09-25 23:39:02 -0300

    Make the #replay button change the display of the cards to none

--- js/base.js
+++ js/base.js
@@ -1,3 +1,7 @@
-$('.card').on('click', function (evt){
+$('.card').on('click', function (){
   $(this).find('.color-image').css('display', 'block')
+})
+
+$('#replay').on('click', function(){
+  $('.color-image').css('display','none')
 })
